eb732ee1238a553585ac38b98dc8c4e96358f0b9
Blake <Blakeathompson94@gmail.com>
2020-04-19 18:37:25 -0400

    random password generation is working, however my box size isnt relative to passwords of a certain length, curious as to how to make that happen

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -4,7 +4,7 @@ const lengthEl = document.getElementById("length");
 const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
 const numbersEL = document.getElementById("numbers");
-const symbolEl = document.getElementById("symbols");
+const symbolsEl = document.getElementById("symbols");
 const generateEl = document.getElementById("generate");
 
 const randomFunc = {
@@ -22,10 +22,62 @@ generateEl.addEventListener("click", () => {
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEL.checked;
   const hasSymbol = symbolsEl.checked;
+  // console.log(hasLower, hasUpper, hasNumber, hasSymbol, length);
 
-  generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
 
-})
+});
+// Generate Password function
+function generatePassword(lower, upper, number, symbol, length) {
+  // intialize password generation
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+
+  console.log("typesCount", typesCount);
+  // Filter out unchecked queries
+  const typesArr = [{
+    lower
+  }, {
+    upper
+  }, {
+    number
+  }, {
+    symbol
+  }].filter(
+    item => Object.values(item)[0]
+  );
+  console.log("typesArr", typesArr);
+
+  if (typesCount === 0) {
+    return "";
+
+  }
+  // Loop over the length of the array for each type of attribute selected
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      // console.log("funcName: ", funcName);
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  console.log(generatedPassword);
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword
+
+}
+
+
+
 
 // Generator Functions
 function getRandomLower() {
@@ -42,5 +94,5 @@ function getRandomNumber() {
 
 function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols[Math.floor(math.random() * symbols.length)];
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
